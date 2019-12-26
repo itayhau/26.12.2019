d43ed76838c5aac1a8940bd0f81537040a468977
itayhau <itayhau@gmail.com>
2019-12-26 11:00:52 +0200

    Create EF.cs

---
+++ EF.cs
@@ -0,0 +1,21 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                Console.WriteLine("Print all: ");
+                entities.Employees.ToList().ForEach(r => Console.WriteLine(JsonConvert.SerializeObject(r)));
+            }
+        }
+    }
+}
